7023ef0825fe861f2d33396d0e026ca09e0202af
Mike Mortensen <ZhakalenDk@users.noreply.github.com>
2021-03-03 02:43:02 +0100

    Uploading extended

--- Blackjack_H2_2021/Scripts/BlackJack.js
+++ Blackjack_H2_2021/Scripts/BlackJack.js
@@ -1,10 +1,10 @@
 //#region Class declarations
 //// Defines a card from a cardgame //////////////////////////
 //////////////////////////////////////////////////////////////
-//////// id: The ID that identifies the card /////////////////
-//////// points: The value of a card         /////////////////
-//////// name: The name of the card          /////////////////
-//////// imageUrl: The releative path to the /////////////////
+//////// _id: The ID that identifies the card ////////////////
+//////// _points: The value of a card         ////////////////
+//////// _name: The name of the card          ////////////////
+//////// _imageUrl: The releative path to the ////////////////
 ////////           image for the card        /////////////////
 //////////////////////////////////////////////////////////////
 class Card {
@@ -14,7 +14,6 @@ class Card {
         this.Name = _name;
         this.ImageUrl = _imageUrl;
 
-        //////////////////////////////////////////////////////////////
         //// Returns a formated string representation ////////////////
         //// of this card                             ////////////////
         //////////////////////////////////////////////////////////////
@@ -32,7 +31,7 @@ class CardHolder {
 
         //// Set the src attribute of the <img> element //////////////
         //////////////////////////////////////////////////////////////
-        //////// imageUrl: The relative path to the //////////////////
+        //////// _imageUrl: The relative path to the /////////////////
         ////////           image for the reference  //////////////////
         //////////////////////////////////////////////////////////////
         this.SetSource = function (_imageUrl) {
@@ -56,13 +55,13 @@ class Deck {
 
         //// Returns the court card value at the specifed index //////
         //////////////////////////////////////////////////////////////
-        //////// index: The zero-based index /////////////////////////
+        //////// _index: The zero-based index ////////////////////////
         ////////        in the collection    /////////////////////////
         //////////////////////////////////////////////////////////////
         this.GetCourtCardValue = function (_index) { return courtCards[_index]; };
-        //// Returns the suit card value at the specifed index //////
+        //// Returns the suit card value at the specifed index ///////
         //////////////////////////////////////////////////////////////
-        //////// index: The zero-based index /////////////////////////
+        //////// _index: The zero-based index ////////////////////////
         ////////        in the collection    /////////////////////////
         //////////////////////////////////////////////////////////////
         this.GetSuitCardValue = function (_index) { return suitCards[_index]; };
@@ -149,8 +148,8 @@ class Deck {
 
 //// Represents a human //////////////////////////////////////
 //////////////////////////////////////////////////////////////
-//////// id: The ID that identifies the person ///////////////
-//////// name: The name of the person          ///////////////
+//////// _id: The ID that identifies the person ////////////// 
+//////// _name: The name of the person          //////////////
 //////////////////////////////////////////////////////////////
 class Person {
     constructor (_id, _name){
@@ -162,8 +161,8 @@ class Person {
 //// Represents a player that can participate in /////////////
 //// the game                                    /////////////
 //////////////////////////////////////////////////////////////
-//////// id: The ID that identifies the player ///////////////
-//////// name: The name of the player          ///////////////
+//////// _id: The ID that identifies the player //////////////
+//////// _name: The name of the player          //////////////
 //////////////////////////////////////////////////////////////
 class Player extends Person{
     constructor (_id, _name){
@@ -182,22 +181,27 @@ class Player extends Person{
 //#endregion
 
 //#region Game Logic
+//#region Data Fields
 ////////////////////// Game UI ///////////////////////////////
 //////////////////////////////////////////////////////////////
-let button = document.getElementById ("btn_draw");
-let txtArea = document.getElementById ("txt_gameInfo");
-let aceValue = document.getElementById ("btn_switchValue");
-let dealerPoints = document.getElementById ("dealerPoints");
-let playerPoints = document.getElementById ("playerPoints");
+let drawButton = document.getElementById ("btn_draw");          //  The button to press when drawing a card
+let stopButton = document.getElementById("btn_stop");        //  The button to press when stopping players turn
+let txtArea = document.getElementById ("txt_gameInfo");         //  The text area that contains the games progress
+let aceValue = document.getElementById ("btn_switchValue");     //  The button to press to switch the value between 1 and 11 for Aces
+let dealerPoints = document.getElementById ("dealerPoints");    //  The dealers scoreboard
+let playerPoints = document.getElementById ("playerPoints");    //  The players scoreboard
 
 ////////////////////// Game Data ////////////////////////////
 /////////////////////////////////////////////////////////////
-var ph = new CardHolder();  //  The HTML card placeholder
-var deck = new Deck();      //  The deck of cards
+var ph = new CardHolder();                  //  The HTML card placeholder
+var deck = new Deck();                      //  The deck of cards
 var dealer = new Player (0, "Dealer");
 var player = new Player (1, "Player");
-let currentCard;            //  The Currently displayed card
-let accepted = false;       //  Whether or not the player accepted the value of an Ace
+let currentCard;                            //  The Currently displayed card
+let accepted = false;                       //  Whether or not the player accepted the value of an Ace
+let playersTurn = true;                     //  Whether or not the current turn is the players or the dealers
+let gameOver = false;
+//#endregion
 
 //#region Initial Setup
 txtArea.innerHTML = "Welcome to Oiski's Blackjack Table!\n";
@@ -213,56 +217,154 @@ deck.Shuffle();
 ////////////////////////////////////////////////////////////
 function DrawCard ()
 {
-    if (button.innerHTML != "Try Again"){
-        if (button.innerHTML != "Accept"){
+    if (drawButton.innerHTML != "Try Again"){
+        if (drawButton.innerHTML != "Accept"){
             currentCard  = deck.DrawCard();
             ph.SetSource (currentCard.ImageUrl);
             
-            txtArea.scrollTop = txtArea.scrollHeight;
-            txtArea.innerHTML += "Player drew: " + currentCard.Name + "\n";
-            
-            if (currentCard.Name.includes("Ace")){
-                txtArea.scrollTop = txtArea.scrollHeight;
-                txtArea.innerHTML += "Ace Counts as: " + aceValue.innerHTML.replace("Ace Value: ", "") + "\n";
-                aceValue.disabled = false;
-                button.innerHTML = "Accept";
+            if (playersTurn == true){
+                AddInfoToGameBoard(player)
             }
             else{
-                aceValue.disabled = true;
+             AddInfoToGameBoard (dealer);   
             }
+            
+            CheckIfAce();
         }
         else{
-            accepted = true;
-            button.innerHTML = "Draw Card";
-            aceValue.disabled = true;
+            OnAccept();
         }
 
         if (currentCard.Name.includes("Ace") == false || accepted == true){
-            player.Points += currentCard.Points;
-            playerPoints.innerHTML = player.Name + " Points: " + player.GetPointsAsDoubleDigitString();
-            accepted == false;
+            if (playersTurn){
+                CountPoints(player);
+                DisplayScore(playerPoints, player);
+            }
+            else{
+                CountPoints (dealer);
+                DisplayScore(dealerPoints, dealer);
+            }
         }
     }
     else{
-        txtArea.scrollTop = txtArea.scrollHeight;
-        txtArea.innerHTML = "";
-
-        ph.SetSource ("./Images/GameBoard.jpg");
-        deck.BuilDeck();
-        deck.Shuffle();
-
-        button.innerHTML = "Draw Card";
+        ResetGame();
     }
-     
-     if (deck.GetCards().length == 0)
-     {
-        button.innerHTML = "Try Again";
-        txtArea.scrollTop = txtArea.scrollHeight;
-        txtArea.innerHTML += "Game Over\n";
-     }
+
+    if (playersTurn == true)
+    {
+        EndGame (player.Points, "Player lost!");
+    }
+    else{
+        EndGame (dealer.Points, "Player won!");
+    }
+
 }
 
-//////////Switches the value of the displayed Ace ////////////
+//// Enable or disable the Ace Value Button///////////////////
+//////////////////////////////////////////////////////////////
+function CheckIfAce (){
+    if (currentCard.Name.includes("Ace")){
+        txtArea.scrollTop = txtArea.scrollHeight;
+        txtArea.innerHTML += "Ace Counts as: " + aceValue.innerHTML.replace("Ace Value: ", "") + "\n";
+        aceValue.disabled = false;
+        drawButton.innerHTML = "Accept";
+    }
+    else{
+        aceValue.disabled = true;
+    }
+}
+
+//// Add the games progress to the gameboard //////////////////////////
+//////////////////////////////////////////////////////////////
+//////// _player: The current playing player /////////////////
+//////////////////////////////////////////////////////////////
+function AddInfoToGameBoard (_player){
+    txtArea.scrollTop = txtArea.scrollHeight;
+    txtArea.innerHTML +=  _player.Name + " drew: " + currentCard.Name + "\n";
+}
+
+//// Display the score of _player on the UI //////////////////
+//////////////////////////////////////////////////////////////
+//////// _scoreboard: The UI element that     ////////////////
+////////              will hold the score     ////////////////
+////------------------------------------------------------////
+//////// _player: The player that hold the    ////////////////
+////////          points to display           ////////////////
+//////////////////////////////////////////////////////////////
+function DisplayScore (_scoreboard, _player)
+{
+    _scoreboard.innerHTML = _player.Name + " Points: " + _player.GetPointsAsDoubleDigitString();
+}
+
+//// Count the points for the current card into //////////////
+///  _players points                            //////////////
+//////////////////////////////////////////////////////////////
+//////// _player: the player to add points for ///////////////
+//////////////////////////////////////////////////////////////
+function CountPoints (_player){
+    _player.Points += currentCard.Points;
+    accepted == false;
+}
+
+//// When the user accepts the value of an Ace ///////////////
+//////////////////////////////////////////////////////////////
+function OnAccept ()
+{
+    accepted = true;
+    drawButton.innerHTML = "Draw Card";
+    aceValue.disabled = true;
+}
+
+//// Ends the game ///////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+//////// _points: the points to check against ////////////////
+////------------------------------------------------------////
+//////// _gameOverMessage: The message to     ////////////////
+///////                    display when the   ////////////////
+///////                    game ends          ////////////////
+//////////////////////////////////////////////////////////////
+function EndGame (_points, _gameOverMessage)
+{
+    if (deck.GetCards().length == 0 || _points > 21)
+    {
+       drawButton.innerHTML = "Try Again";
+       txtArea.scrollTop = txtArea.scrollHeight;
+       txtArea.innerHTML += _gameOverMessage + "\n";
+       stopButton.disabled = true;
+       return true;
+    }
+
+    return false;
+}
+
+////////// Resets the game to its initial setup //////////////
+//////////////////////////////////////////////////////////////
+function ResetGame ()
+{
+    console.log("<----Game was reset---->");
+    txtArea.scrollTop = txtArea.scrollHeight;
+    txtArea.innerHTML = "";
+    stopButton.disabled = false;
+    player.Points = 0;
+    dealer.Points = 0;
+    playerPoints.innerHTML = player.Name + "Points: " + player.GetPointsAsDoubleDigitString();
+    dealerPoints.innerHTML = dealer.Name + "Points: " + dealer.GetPointsAsDoubleDigitString();
+    playersTurn = true;
+
+    console.log ("      " + player.Name + " points: " + player.Points);
+    console.log ("      " + dealer.Name + " points: " + dealer.Points);
+    console.log ("      Player scoreboard: " + playerPoints.innerHTML);
+    console.log ("      Dealer scoreboard: " + dealerPoints.innerHTML);
+    console.log ("      Players turn: " + playersTurn);
+
+    ph.SetSource ("./Images/GameBoard.jpg");
+    deck.BuilDeck();
+    deck.Shuffle();
+
+    drawButton.innerHTML = "Draw Card";
+}
+
+////////// Switches the value of the displayed Ace ///////////
 //////////////////////////////////////////////////////////////
 function SwitchValue (){  
     if (aceValue.innerHTML == "Ace Value: 01"){
@@ -275,5 +377,22 @@ function SwitchValue (){
     }
     txtArea.scrollTop = txtArea.scrollHeight;
     txtArea.innerHTML += "Ace Counts as: " + aceValue.innerHTML.replace("Ace Value: ", "") + "\n";
+}
+
+////////// Stop the current turn and hand it over ////////////
+////////// to the other player                    ////////////
+//////////////////////////////////////////////////////////////
+function StopTurn (){
+    playersTurn = !playersTurn;
+
+    txtArea.scrollTop = txtArea.scrollHeight;
+    if (playersTurn == true){
+        txtArea.innerHTML +=  "Turn given to: " + player.Name + "\n";
+    }
+    else{
+        txtArea.innerHTML +=  "Turn given to: " + dealer.Name + "\n";
+    }
+
+    stopButton.disabled = true;
 }
 //#endregion
