374faff9ba9adf0e322be8eb3566b11713dc7eb1
Mike Mortensen <ZhakalenDk@users.noreply.github.com>
2021-03-04 14:49:02 +0100

    Added a timer class

--- Blackjack_H2_2021/Scripts/Initiate.js
+++ Blackjack_H2_2021/Scripts/Initiate.js
@@ -29,3 +29,71 @@ function ToIndex (){
 function ToGamePage (){
     window.location.href = "./gamePage.html";
 }
+
+//// Represents a timer that can be started and stopped ///////
+//////////////////////////////////////////////////////////////
+class Timer{
+    constructor (){
+        var seconds = 0;
+        var minutes = 0;
+        var hours = 0;
+        var run = true;
+
+        this.GetSeconds = function (){
+            return seconds;
+        }
+
+        this.GetMinutes = function (){
+            return minutes;
+        }
+
+        this.GetHours = function (){
+            return hours;
+        }
+
+        this.GetSecondsWithDigets = function (_digits = 2){
+            return seconds.toLocaleString("en-US", { minimumIntegerDigits: _digits, useGrouping: false});
+        }
+
+        this.GetMinutesWithDigets = function (_digits = 2){
+            return minutes.toLocaleString("en-US", { minimumIntegerDigits: _digits, useGrouping: false});
+        }
+
+        this.GetHoursWithDigets = function (_digits = 2){
+            return hours.toLocaleString("en-US", { minimumIntegerDigits: _digits, useGrouping: false});
+        }
+        
+        this.GetAsString = function (){
+            return `${this.GetHoursWithDigets(2)}:${this.GetMinutesWithDigets(2)}:${this.GetSecondsWithDigets(2)}`;
+        }
+
+        this.Start = async function (_shouldLog = false){
+            run = true;
+            do {
+                if (run == false){
+                    break;
+                }
+                if (_shouldLog == true){
+                    console.log(this.GetAsString());
+                }
+                await new Promise (resolve => {setTimeout (resolve, 1000)});
+                seconds++;
+
+                if (seconds == 60){
+                    seconds = 0;
+                    minutes++;
+            
+                    if (minutes == 60){
+                        minutes = 0;
+                        hours++;
+                    }
+                }
+            }while (true);
+        }
+
+        this.Stop = function (_shouldLog){
+            run = false;
+            console.log ("Timer Stopped");
+        }
+    }
+}
